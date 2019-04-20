b9d8ace0b7d68ab0e3a53303884a70f5fc2653c7
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-20 18:03:56 +0700

    make model

---
+++ app/model.py
@@ -0,0 +1,7 @@
+class Model:
+
+    def __init__(self, room_id, message, message_type, sender):
+        self.room_id = room_id
+        self.message = message
+        self.message_type = message_type
+        self.sender = sender
