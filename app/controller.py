58adddac870b1c1d029a2209356dfeb1f0f6d2f9
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-20 21:59:12 +0700

    make controller

--- app/controller.py
+++ app/controller.py
@@ -1,11 +1,23 @@
 import unirest
 from model import *
-from flask import request, jsonify
+from flask import request, jsonify, abort
 from array import *
 
 class Controller:
 
     def __init__(self):
         self.access_token = ""
-        self.apiurl = ""
-        self.headers = array()
+        self.apiurl = "https://qisme.qiscus.com/api/v1/chat/conversations/"
+        self.headers = {"Content-Type" : "application/json"} 
+        self.qismeResponse = ""
+
+    def getResponse():
+        if request.method == "POST":
+            self.qismeResponse = request.json
+            print(self.qismeResponse)
+            return '', 200
+        else:
+            abort(400)
+
+    def run():
+        self.getResponse()        
