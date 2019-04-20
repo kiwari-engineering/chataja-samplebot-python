9d1acbca33ed238613a5d05ba3a1a6c50192e709
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-21 01:00:14 +0700

    add log message

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,6 @@
 # import unirest
 # import model
+import simplejson
 from flask import request, jsonify, abort
 from array import *
 
@@ -12,8 +13,10 @@ class Controller:
     def getResponse():
         if request.method == "POST":
             qismeResponse = request.json
-            print(qismeResponse)
-            return '', 200
+            log = open("log-comment.txt", "w")
+            log.write(simplejson.dumps(qismeResponse, indent=4, sort_keys=True))
+            log.close()
+            return jsonify({"status":"success"}), 200
         else:
             abort(400)
 
