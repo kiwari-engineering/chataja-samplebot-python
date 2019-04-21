ac304e9a2ca1bb73e1823d7b7e596e296499c37b
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-21 19:04:01 +0700

    make controller

--- app/controller.py
+++ app/controller.py
@@ -7,7 +7,6 @@ from array import *
 class Controller:
     access_token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0Njk1NSwidGltZXN0YW1wIjoiMjAxOS0wNC0wNSAwNjoxMjo0MSArMDAwMCJ9.u5PHjfNPrRL_nhh5S-UUSNLBr2kKBlBI89px2L2jjdg"
     apiurl = "https://qisme.qiscus.com/api/v1/chat/conversations/post_comment"
-    headers = {"Accept" : "application/json"} 
     qismeResponse = ""
     http = urllib3.PoolManager()
 
@@ -30,11 +29,48 @@ class Controller:
             "type" : "text",
             "comment" : comment 
         }
-        text = Controller.http.request("POST", Controller.apiurl, headers=Controller.headers, fields=replay)
+        text = Controller.http.request("POST", Controller.apiurl, fields=replay)
         if text.status == 200:
             return jsonify({"status":"success"}), text.status
         else:
-            return jsonify({"status":"error"}), text.status   
+            return abort(text.status)   
+
+    #contoh penggunaan api post-comment untuk jenis button
+    def replyCommandButton(display_name, room_id):
+        comment = "Halo {}, ini adalah contoh payload button yang bisa kamu coba".format(display_name)
+        payload = {
+            "text" : comment,
+            "buttons" : [
+                {
+                    "label" : "Tombol Reply Text",
+                    "type" : "postback",
+                    "payload" : {
+                        "url" : "#",
+                        "method" : "get",
+                        "payload" : "null"
+                    }
+                },
+                {
+                    "label" : "Tombol Link",
+                    "type" : "link",
+                    "payload" : {
+                        "url" : "https://www.google.com",
+                    }
+                }
+            ]
+        }
+        encoded_data = simplejson.dumps(payload).encode('utf-8')
+        replay = { 
+            "access_token" : Controller.access_token,
+            "topic_id" : room_id,
+            "type" : "buttons",
+            "payload" : encoded_data
+        }
+        buttons = Controller.http.request("POST", Controller.apiurl, fields=replay)
+        if buttons.status == 200:
+            return jsonify({"status":"success"}), buttons.status
+        else:
+            return jsonify({"status":"error"}), buttons.status    
 
     def run():
         Controller.getResponse() 
@@ -44,5 +80,11 @@ class Controller:
             Controller.qismeResponse["message"]["type"],
             Controller.qismeResponse["from"]["fullname"]
         ) 
-        return Controller.replyCommandText(data.sender, data.message_type, data.room_id)
+        # if not (data.message is None):
+        #     if data.message[0] == "/":
+        #         command = data.message.split("/")
+        #         if not (command[1] is None):
+
+        return Controller.replyCommandButton(data.sender, data.room_id)
+        #return Controller.replyCommandText(data.sender, data.message_type, data.room_id)
         # return jsonify({"status":"success"}), 200          
