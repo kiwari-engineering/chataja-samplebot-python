e09a4f40b802bb90e316dde647ee03cf79e4efa8
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-21 21:19:02 +0700

    make model

--- app/model.py
+++ app/model.py
@@ -1,5 +1,5 @@
 class Model:
-
+    #model untuk menampung response data dari webhook
     def __init__(self, room_id, message, message_type, sender):
         self.room_id = room_id
         self.message = message
