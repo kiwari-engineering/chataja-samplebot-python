fa26209faa76e7c4f21cae40c7d0c3feb282110e
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-21 21:20:43 +0700

    make app

--- index.py
+++ index.py
@@ -2,10 +2,10 @@
 # @email: hello@iskandarsuhaimi.com
 
 from flask import Flask
-from app.controller import Controller
+from app.controller import Controller #panggil controller
 
-app = Flask(__name__)
+app = Flask(__name__) #inisiasi aplikasi flask
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['POST']) #jalankan bot pada url root ('/')
 def run():
-    return Controller.run()
+    return Controller.run() #panggil method run controller
